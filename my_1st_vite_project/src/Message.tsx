063ef0dc8a728f702e377834ddd234336dc618dd
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-06-11 13:00:15 +0530

    rename = Move

--- my_1st_vite_project/src/Message.tsx
+++
@@ -1,8 +0,0 @@
-// Y:PascalCassing
-function MessageNew() {
-  const UserName = 'Mukul';
-  if (UserName) return <h1>Hello {UserName}</h1>;
-  else return <h1>Hello REACT</h1>;
-}
-
-export default MessageNew; // IMP: no () at function export.
